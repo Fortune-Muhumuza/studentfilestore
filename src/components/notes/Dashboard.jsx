67511ee3bb4b10dc7b9577f7927f8d3c7421af57
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2022-05-30 21:55:17 +0300

    added class notes component

--- src/components/notes/Dashboard.jsx
+++ src/components/notes/Dashboard.jsx
@@ -1,25 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Dashboard.module.css";
-import NotesCard from "../UI/NotesCard";
-import { notes } from "../dummyData";
+import NotesCard from "../UI/cards/NotesCard";
+import { notes } from "../admin/dummyData";
 
 const Dashboard = () => {
-  const [notesList, setNotesList] = useState(notes);
-  
-
   return (
     <div className={styles.dashboardContainer}>
       <div className={styles.dashboardHeader}>
         <h1>Dashboard</h1>
       </div>
       <div className={styles.dashboardContents}>
-        {notesList?.map((note) => (
+        {notes?.map((note) => (
           <NotesCard
-          key={note.id}
+            key={note.id}
             title={note.title}
             description={note.description}
             format={note.format}
             id={note.id}
+            isDashboard={true}
           />
         ))}
       </div>
