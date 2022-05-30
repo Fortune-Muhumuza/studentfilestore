01b5d597e133f4ade212c40814309525785cccdf
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2022-05-30 21:56:02 +0300

    added dummy data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import { Routes, Route,  } from "react-router-dom";
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import Sidebar from './components/UI/Sidebar'
-import Dashboard from './components/notes/Dashboard'
-import MyNotes from './components/notes/MyNotes'
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/UI/navigation/Sidebar";
+import Dashboard from "./components/notes/Dashboard";
+import MyNotes from "./components/notes/MyNotes";
 import ClassNotes from "./components/notes/ClassNotes";
 import AddNotes from "./components/notes/AddNotes";
+import Header from "./components/UI/navigation/Header";
+import Statistics from "./components/admin/Statistics";
 
 function App() {
   return (
-    <div className='mainContainer'>
+    <div className="mainContainer">
       <div>
-    <Sidebar />
-    </div>
-    <div>
-    <Routes>
-    {/* <div className='container'> */}
-    {/* <Route path='/myNotes' element={MyNotes} /> */}
-    <Route path='/' element={<Dashboard />} />
-    <Route path='/myNotes' element={<MyNotes />} />
-    
-    <Route path='/classNotes' element={<ClassNotes />} />
-    <Route path='/addNotes' element={<AddNotes />} />
-    
+        <Sidebar />
+      </div>
+     
+      <div className="mainContents">
+        <Header />
+      
+        <Routes>
+          {/* <div className='container'> */}
+          {/* <Route path='/myNotes' element={MyNotes} /> */}
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/myNotes" element={<MyNotes />} />
 
-    {/* </div> */}
-    </Routes>
+          <Route path="/classNotes" element={<ClassNotes />} />
+          <Route path="/addNotes" element={<AddNotes />} />
+          <Route path="/stats" element={<Statistics />} />
+
+          {/* </div> */}
+        </Routes>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
