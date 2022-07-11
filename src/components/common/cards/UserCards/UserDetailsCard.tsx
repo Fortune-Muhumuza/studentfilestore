2ed90bb790ba422a3d1b8487b3b4a1c35f80383f
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2022-07-11 23:37:07 +0300

    refactor: convert user card to ts

--- src/components/common/cards/UserCards/UserDetailsCard.tsx
+++ src/components/common/cards/UserCards/UserDetailsCard.tsx
@@ -1,23 +1,36 @@
 import React from "react";
-const styles =  require ('./UserDetailsCard.module.css');
+const styles = require("./UserDetailsCard.module.css");
 
-type Props = {
-  onClick: MouseEventHandler,
-  text: string,
-}
+type props = {
+  // onClick: MouseEventHandler;
+  text: string;
+  firstName: string;
+  secondName: string;
+  role: string;
+  email: string;
+  rating: string;
+  profilePicture: string;
+};
 
-const UserDetailsCard = (props) => {
+const UserDetailsCard = ({
+  firstName,
+  secondName,
+  role,
+  email,
+  rating,
+  profilePicture
+}: props) => {
   return (
     <div className={styles.cardContainer}>
-      <div className={styles.cardHeader}>
-        <img src={props.profilePicture} alt="" className={styles.cardPicture} />
+      <div className={styles.cardHeader}> 
+        <img src={profilePicture} alt="" className={styles.cardPicture} />
       </div>
       <div className={styles.cardContents}>
-        <p>Firstname: {props.firstName}</p>
-        <p>Secondname: {props.secondName}</p>
-        <p>Role: {props.role}</p>
-        <p>Email: {props.email}</p>
-        <p>Rating: {props.rating}</p>
+        <p>Firstname: {firstName}</p>
+        <p>Secondname: {secondName}</p>
+        <p>Role: {role}</p>
+        <p>Email: {email}</p>
+        <p>Rating: {rating}</p>
       </div>
       <div className={styles.cardFooter}></div>
     </div>
